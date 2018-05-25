c719a1e08ae65552831a303b529490b1d8b92c79
hdcardenas2 <dropimax@gmail.com>
2018-05-24 21:42:22 -0500

    bd

---
+++ src/main/resources/bd/test.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-05-2018 a las 04:41:51
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `test`
+--
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `test`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detail_users_roll`
+--
+
+DROP TABLE IF EXISTS `detail_users_roll`;
+CREATE TABLE `detail_users_roll` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `roll_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `detail_users_roll`
+--
+
+INSERT INTO `detail_users_roll` (`id`, `user_id`, `roll_id`) VALUES
+(3, 1, 287);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roll`
+--
+
+DROP TABLE IF EXISTS `roll`;
+CREATE TABLE `roll` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `roll`
+--
+
+INSERT INTO `roll` (`id`, `name`) VALUES
+(286, 'Administrador SIUT'),
+(287, 'Solicitante SIUT'),
+(288, 'Revisión BPY'),
+(289, 'Aprueba BPY'),
+(290, 'Reparto JRD'),
+(291, 'Revisor final JRD');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `password`) VALUES
+(1, 'test', 'dGVzdA==');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `detail_users_roll`
+--
+ALTER TABLE `detail_users_roll`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_roll_1` (`roll_id`),
+  ADD KEY `fk_user_1` (`user_id`);
+
+--
+-- Indices de la tabla `roll`
+--
+ALTER TABLE `roll`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `detail_users_roll`
+--
+ALTER TABLE `detail_users_roll`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `roll`
+--
+ALTER TABLE `roll`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=292;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detail_users_roll`
+--
+ALTER TABLE `detail_users_roll`
+  ADD CONSTRAINT `fk_roll_1` FOREIGN KEY (`roll_id`) REFERENCES `roll` (`id`),
+  ADD CONSTRAINT `fk_user_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
